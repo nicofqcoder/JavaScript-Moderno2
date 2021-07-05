3dcd54f7815cd39b614df474bb6e6945ca3003dd
Nicolas F <nicolas.farreaut@gmail.com>
2021-07-04 20:12:14 -0400

    Primeros archivos del curso

---
+++ 02/js/app.js
@@ -0,0 +1,8 @@
+// Pregunta al usuario su nombre
+const nombre = prompt('Cuál es tu nombre?');
+
+// Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript Moderno`;
+
+// Asigna el valor hacia la variable producto
+const producto = 'Monitor 24 pulgadas';
---
+++ 03/js/app.js
@@ -0,0 +1,30 @@
+// Inicializar una variable con un valor
+var producto = "Monitor de 24 Pulgadas";
+console.log(producto);
+
+// Las variables se pueden reasignar
+producto = 'Monitor de 19 pulgadas';
+console.log(producto);
+
+// JavaScript es un lenguaje de tipo denámico, no se especifíca tipo de dato
+producto = 20;
+console.log(producto);
+
+// Se pueden inicializar sin valor y asignarlo después
+var disponible;
+disponible = true;
+
+disponible = false;
+
+// Inicializar múltiples variables.
+var categoria = 'Computadoras',
+    marca     = ' Marca Famosa',
+    calificacion = 5;
+// No puede inicializar una variable con número
+
+// CamelCase 
+var nombreProductos;
+// SnakeCase
+var nombre_producto_categoria
+// PascalCase
+var Producto
